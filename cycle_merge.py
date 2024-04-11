3621a1ed30662a307bc4cd52fa4f2b709d728c4e
Cdrow59 <Crd@drownfamily.com>
2024-04-10 23:24:02 -0400

    2.0 rewrite with seeding

--- cycle_merge.py
+++ cycle_merge.py
@@ -17,56 +17,47 @@ redirect_uri = 'http://localhost:8000/callback'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope='playlist-modify-public',cache_path=".cache"))
 
 #Define Functions
-def custom_sort(lst):
-    # Separate the list into groups based on the first element of each tuple
-    grouped = {}
-    for item in lst:
-        index = item[0]
-        grouped.setdefault(index, []).append(item)
+def custom_sort(target):
+    #define result
+    result = []
+    random.seed(a=seed ,version=2)
+    # Loop through each key in the dictionary
+    loop = True
+    while loop == True:
+        for key in target:
+            # Check if the list associated with the key is not empty
+            if target[key]:
+                # Randomly choose a value from the list
+                random_value = random.choice(target[key])
+                result.append(random_value)
+                # Remove the chosen value from the list
+                target[key].remove(random_value)
+            else:
+                loop = False
+                break
+    return result
 
-    # Create a list to store the sorted result
-    sorted_list = []
+#Configure Script
+seed = 0
 
-    # Alternate between groups and extend the sorted list
-    if remove_extra == True:
-        # Determine the minimum length among the groups
-        min_len = min(len(grouped[index]) for index in grouped)
-        
-        for i in range(min_len):
-            for index in sorted(grouped):
-                sorted_list.append(grouped[index][i])
-    else:
-        # Determine the maximum length among the groups
-        max_len = max(len(grouped[index]) for index in grouped)
-        
-        # Alternate between groups and extend the sorted list
-        for i in range(max_len):
-            for index in sorted(grouped):
-                if i < len(grouped[index]):
-                    sorted_list.append(grouped[index][i])
-                    
-    return sorted_list
-
-def random_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        j = random.randint(0, n-1)
-        arr[i], arr[j] = arr[j], arr[i]
-    return arr
-
-def extract_second_elements(list_of_tuples):
-    return [tup[1] for tup in list_of_tuples]
-
-shuffle = True
-remove_extra = False
 playlists = [
-    "spotify:playlist:0fP2GmLGb5FCS4GvBiS3lu",
+    "spotify:playlist:5DDg9WcDDC54z69Y51KSmW",
     "spotify:playlist:0w4vurwPbF5sv6tUEBEsWK",
-    "spotify:playlist:5DDg9WcDDC54z69Y51KSmW"
+    "spotify:playlist:0fP2GmLGb5FCS4GvBiS3lu",
+    "spotify:playlist:3mybWjdM3iddZDoB00XGJs"
 ]
-data = []
-print("PLease wait this may take a while!")
+
+# Initialize an empty dictionary to store the data
+data = {}
+
+# Initialize an list dictionary to store sorted track ids
+track_ids = []
+
+# Initialize a set to store all added track IDs
+added_track_ids = set()
+
 # Iterate over playlists
+print("Please wait this may take a while!")
 for index, playlist in enumerate(playlists):
     
     # Retrieve the first page of tracks in the playlist
@@ -83,21 +74,29 @@ for index, playlist in enumerate(playlists):
     for item in playlist_tracks:
         track = item['track']
         track_id = track['id']
-        data.append((index, track_id))
-
-# Shuffle Logic and Calling Sorts
-if shuffle == True:
-    rand = random_sort(data)
-    sort = custom_sort(rand)
-else:
-    sort = custom_sort(data)
-
-#Remove First/Tuple element after sorting
-track_uris = extract_second_elements(sort)
-checked_track_uris = [uri for uri in track_uris if uri is not None]
+        
+        if track_id is None or track_id in added_track_ids:  # Check if the track ID is None or already in the set
+            continue  # Skip processing if it meets the condition
+        
+        # Check if the index is already a key in the dictionary
+        if index in data:
+            # If the index exists, append the track_id to the list of track_ids
+            data[index].append(track_id)
+            # Add the track ID to the set of added track IDs
+            added_track_ids.add(track_id)
+        else:
+            # If the index does not exist, create a new key-value pair
+            data[index] = [track_id]
+            # Add the track ID to the set of added track IDs
+            added_track_ids.add(track_id)
+            
+#Define Copy
+datacopy = data
+#Call Functions
+track_ids = custom_sort(datacopy)
 
 #Chunking Setup
-a_list = checked_track_uris
+a_list = track_ids
 chunked_list = list()
 chunk_size = 10000
 n_chunks = 0
